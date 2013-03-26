e08c66d7bdb0241dc351fa1ff8180f103ea899dc
unknown <ch005004@techranger116.techrangers.net>
2013-03-26 11:16:25 -0400

    changed dbname in development/db.php and db.php to reflect the proper database names

--- fuel_intro/fuel/app/config/development/db.php
+++ fuel_intro/fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fueltut',
+			'dsn'        => 'mysql:host=localhost;dbname=fuel_intro',
 			'username'   => 'root',
 			'password'   => '',
 		),
